4a70cb995ecfbe88d8cfdf06c19170ae56c04c8b
colina118 <mikehill2010@gmail.com>
2015-10-07 22:32:19 -0500

    tarea3

--- Tarea3/Parejas/main.cpp
+++ Tarea3/Parejas/main.cpp
@@ -1,79 +1,96 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
-template<class T>
-std::vector<int> calcularSub(std::vector<T>, std::vector<T>);
+
+bool prefiere(std::vector<std::vector<int>>, int, int, int, int); 
+
+void parejas(std::vector<std::vector<int>>,  int);
 
 int main()
 {
-	std::vector<int> primera = { 1,3,6,12,45,78,96,2,4,87,34,5 };
-	std::vector<int> segunda = { 1,54,7,9,2,3,4,5,6 };
-	std::vector<int> subsecuencia;
+	int n = 4;
+	std::vector<std::vector<int>> preferencias(2 * n, std::vector<int>(n));
+	preferencias = { { 6, 5, 4, 7 },
+	{ 5, 7, 4, 6 },
+	{ 6, 4, 7, 5 },
+	{ 4, 5, 6, 7 },
+	{ 0, 2, 1, 3 },
+	{ 2, 3, 1, 0 },
+	{ 1, 2, 3, 0 },
+	{ 3, 1, 0, 2 },
+	};
+	parejas(preferencias, n);
 
-	subsecuencia = calcularSub(primera, segunda);
-
-	std::cout << "La subsecuencia mas larga es: { ";
-	for (int i = subsecuencia.size(); i > 0; i--)
-	{
-		std::cout << subsecuencia[i-1];
-	}
-
-	std::cout << " }" << std::endl;
-	return 0;
 }
 
-template<class T>
-std::vector<int> calcularSub(std::vector<T> primera, std::vector<T> segunda)
+
+void parejas(std::vector<std::vector<int>> preferencias, int n)
 {
-	int tamPrim = primera.size()-1;
-	int tamSeg = segunda.size()-1;
-	std::vector<std::vector<int>> matrizSub(primera.size() + 1, std::vector<int>(segunda.size() + 1));
-	for (int i = 1; i < primera.size(); i++)
+	std::vector<int> disponibilidadMujeres;
+	std::vector<bool> asignadoHombre;
+
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 1; j < segunda.size(); j++)
+		disponibilidadMujeres.push_back(-1);
+		asignadoHombre.push_back(false);
+	}
+
+	int hombresLibres = n;
+
+	while (hombresLibres > 0)
+	{
+		int hombre;
+		for (int i = 0; i < n; i++)
 		{
-			if (primera[i - 1] == segunda[j - 1])
+			if (asignadoHombre[i] == false)
 			{
-				matrizSub[i][j] = matrizSub[i-1][j-1] + 1;
+				hombre = i;
+				i = n;
+			}
+		}
+		for (int i = 0; i < n && asignadoHombre[hombre] == false; i++)
+		{
+			int mujer = preferencias[hombre][i];
+			if (disponibilidadMujeres[mujer - n] == -1)
+			{
+				disponibilidadMujeres[mujer - n] = hombre;
+				asignadoHombre[hombre] = true;
+				hombresLibres--;
 			}
 			else
 			{
-				matrizSub[i][j] = std::max(matrizSub[i - 1][j], matrizSub[i][j - 1]);
+				int hombre1 = disponibilidadMujeres[mujer - n];
+				if (prefiere(preferencias, hombre, mujer, hombre1, n) == false)
+				{
+					disponibilidadMujeres[mujer - n] = hombre;
+					asignadoHombre[hombre] = true;
+					asignadoHombre[hombre1] = false;
+				}
 			}
 		}
 	}
-	for (int i = 1; i < primera.size(); i++)
+
+	std::cout << "Mujer   Hombre" << std::endl;
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = 1; j < segunda.size(); j++)
-		{
-			std::cout << matrizSub[i][j];
-		}
-		std::cout << std::endl;
+		std::cout << " " << i + n << "\t" << disponibilidadMujeres[i] << std::endl;
 	}
 
-	
-	int tam = matrizSub[tamPrim][tamSeg];
-	std::vector<int> subsecuencia;
-	
-	while (tamPrim > -1 && tamSeg > -1)
+}
+
+
+bool prefiere(std::vector<std::vector<int>> preferencias, int hombre, int mujer, int hombre1, int n)
+{
+	for (int i = 0; i < n; i++)
 	{
-		if (primera[tamPrim] == segunda[tamSeg])
+		if (preferencias[mujer][i] == hombre1)
 		{
-			subsecuencia.push_back(primera[tamPrim]);
-			tamPrim--;
-			tamSeg--;
-			tam--;
+			return true;
 		}
-		else if( primera[tamPrim] > segunda[tamSeg])
+		if (preferencias[mujer][i] == hombre)
 		{
-			tamPrim--;
-		}
-		else
-		{
-			tamSeg--;
+			return false;
 		}
 	}
-	return subsecuencia;
 }
